e1035ca9be3c58b90c442e038d525c0e914f5ec2
rabbishuki <rabbishuki@gmail.com>
2017-01-18 01:34:56 +0200

    Added ID to cookies and to user table

--- server.js
+++ server.js
@@ -8,20 +8,22 @@ var currentUser;
 var db = require('./database.js');
 
 var Users = {
-  userList: [
-    {
-      username: 'שמוליק',
-      password: '770',
-      permission: Admin,
-      token: '12345'
-    },
-    {
-      username: 'שלום',
-      password: '770',
-      permission: User,
-      token: '65432'
-    }
-  ]
+    userList: [
+        {
+            id: '1',
+            username: 'שמוליק',
+            password: '770',
+            permission: Admin,
+            token: '12345'
+        },
+        {
+            id: '2',
+            username: 'שלום',
+            password: '770',
+            permission: User,
+            token: '65432'
+        }
+    ]
 }
 
 app.use('/javascript', express.static(__dirname + '/dist/js'));
@@ -33,86 +35,92 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 function requireRole(role) {
-  return function (req, res, next) {
-    var a = false;
-    for (i in role) {
-      var credentials = req.cookies.token ? req.cookies : req.body;
+    return function (req, res, next) {
+        var a = false;
+        for (i in role) {
+            var credentials = req.cookies.token ? req.cookies : req.body;
 
-      if (validate(credentials, role[i])) {
-        a = true;
-        next();
-      }
+            if (validate(credentials, role[i])) {
+                a = true;
+                next();
+            }
+        }
+        if (!a)
+            res.send(403);
     }
-    if (!a)
-      res.send(403);
-  }
 }
 
 function validate(credentials, key) {
-  currentUser = Users.userList.filter(function (value) {
-    return value.token === credentials.token;
-  })[0];
-
-  if (!currentUser) {
     currentUser = Users.userList.filter(function (value) {
-      return (value.username === credentials.username && value.password === credentials.password);
+        return value.token === credentials.token;
     })[0];
-  }
 
-  if (currentUser) return (currentUser.permission === key);
+    if (!currentUser) {
+        currentUser = Users.userList.filter(function (value) {
+            return (value.username === credentials.username && value.password === credentials.password);
+        })[0];
+    }
+
+    if (currentUser) return (currentUser.permission === key);
 }
 
 app.get('/', function (req, res) {
-  console.log('enter');
-  res.sendFile(__dirname + "/dist/views/enter.html");
+    console.log('enter');
+    res.sendFile(__dirname + "/dist/views/enter.html");
 });
 
 app.post('/login', requireRole([Admin, User]), function (req, res) {
-  res.send({
-    token: currentUser.token,
-    link: currentUser.permission
-  });
+    res.send({
+        token: currentUser.token,
+        link: currentUser.permission,
+        UserID: currentUser.id
+    });
 }
 );
 
 app.get('/' + Admin, requireRole([Admin]), function (req, res) {
-  console.log(Admin);
-  res.sendFile(__dirname + '/dist/index.html');
+    console.log(Admin);
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.get('/' + User, requireRole([User]), function (req, res) {
-  console.log(User);
-  res.sendFile(__dirname + '/dist/index.html');
+    console.log(User);
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 
 app.get('/students', requireRole([Admin, User]), function (req, res) {
-  res.send({
-    students: db.DB.students
-  });
+    res.send({
+        students: db.DB.students
+    });
 });
 
 app.post('/students', requireRole([Admin, User]), function (req, res) {
-  if (db.ADD('students', req.body.student)) {
-    res.send({
-      success: 'האברך נוסף בהצלחה'
-    });
-  } else {
-    res.send({
-      error: 'המשתמש כבר קיים'
-    });
-  };
+    // try and save object in database, and send result to client.
+    if (db.ADD('students', req.body.student)) {
+        res.send({
+            success: 'האברך נוסף בהצלחה',
+            students: db.DB.students
+        });
+    } else {
+        res.send({
+            error: 'המשתמש כבר קיים'
+        });
+    };
 });
 
 app.put('/students', requireRole([Admin, User]), function (req, res) {
-  db.UPD('students', req.body.id, req.body.student);
-  res.send({
-    success: 'האברך עודכן בהצלחה'
-  });
+    // Save object in database.
+    db.UPD('students', req.body.id, req.body.student);
+
+    res.send({
+        success: 'האברך עודכן בהצלחה',
+        students: db.DB.students
+    });
 });
 
 var port = process.env.PORT || 8080;
 
 app.listen(port, function () {
-  console.log('Our app is running on http://localhost:' + port);
+    console.log('Our app is running on http://localhost:' + port);
 });
--- src/js/controllers/studentController.js
+++ src/js/controllers/studentController.js
@@ -1,40 +1,38 @@
 angular.module('RDash')
-  .controller('studentController', function ($scope, Data) {
-    Data.get('students').then(function (data) {
-      $scope.students = data.students;
-    })
-
-    $scope.student = {};
-
-    $scope.add = function () {
-      $scope.display = true;
-    };
-
-    $scope.edit = function (id) {
-      $scope.display = true;
-      $scope.student = $scope.students[id];
-      $scope.student.editId = id.toString();
-    }
-
-    $scope.save = function (valid) {
-      if (!valid) {
-        $scope.formErrors = true;
-      } else if (valid) {
-        var method = $scope.student.editId ? 'put' : 'post';
-        Data[method]('students', { id: $scope.student.editId, student: $scope.student }).then(function (result) {
-
-          Data.get('students').then(function (data) {
+    .controller('studentController', function ($scope, Data) {
+        Data.get('students').then(function (data) {
             $scope.students = data.students;
-          })
-        });
+        })
 
-        $scope.close();
-      }
-    }
+        $scope.student = {};
 
-    $scope.close = function () {
-      $scope.student = {};
-      $scope.formErrors = false;
-      $scope.display = false;
-    }
-  });
+        $scope.add = function () {
+            $scope.display = true;
+        };
+
+        $scope.edit = function (id) {
+            $scope.display = true;
+            $scope.student = $scope.students[id];
+            $scope.student.editId = id.toString();
+        }
+
+        $scope.save = function (valid) {
+            if (!valid) {
+                $scope.formErrors = true;
+            } else if (valid) {
+                var method = $scope.student.editId ? 'put' : 'post';
+                
+                Data[method]('students', { id: $scope.student.editId, student: $scope.student }).then(function (result) {
+                    $scope.students = data.students;
+                });
+
+                $scope.close();
+            }
+        }
+
+        $scope.close = function () {
+            $scope.student = {};
+            $scope.formErrors = false;
+            $scope.display = false;
+        }
+    });
