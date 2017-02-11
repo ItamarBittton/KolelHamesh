37d91cd4f3f1ad070099326c2ca869cbcbe7ffcc
rabbishuki <rabbishuki@gmail.com>
2017-02-12 01:10:17 +0200

    FIX: moved all functions from server.js

--- server.js
+++ server.js
@@ -7,25 +7,6 @@ var express = require('express'),
     currentUser,
     f = require('./Server/functions.js');
 
-var Users = {
-    userList: [
-        {
-            id: '0',
-            username: 'שמוליק',
-            password: '770',
-            permission: Admin,
-            token: '12345'
-        },
-        {
-            id: '1',
-            username: 'שלום',
-            password: '770',
-            permission: User,
-            token: '65432'
-        }
-    ]
-}
-
 app.use('/javascript', express.static(__dirname + '/dist/js'));
 app.use('/image', express.static(__dirname + '/dist/img'));
 app.use('/lib', express.static(__dirname + '/dist/lib'));
@@ -39,76 +20,30 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/dist/views/enter.html");
 });
 
-app.get('/' + Admin, requireRole([Admin]), sendHomePage);
-app.get('/' + User, requireRole([User]), sendHomePage);
-
-app.post('/login', requireRole([Admin, User]), sendCookies);
-
-app.get('/students', requireRole([Admin, User]), f.getStudents);
-app.post('/students', requireRole([Admin]), f.newStudent);
-app.put('/students', requireRole([Admin]), f.editStudent);
-app.delete('/students', requireRole([Admin]), f.deleteStudent);
-
-app.get('/recomends', requireRole([Admin, User]), f.getRecomends);
-app.post('/recomends', requireRole([User]), f.newRecomend);
-app.post('/approve', requireRole([Admin]), f.approveRecomend);
-app.post('/deny', requireRole([Admin]), f.denyRecomend);
-
-app.post('/daily', requireRole([Admin, User]), f.getDailyReport);
-//app.put('/daily', requireRole([Admin, User]), f.updateDailyReport);
-
-app.post('/scores', requireRole([Admin, User]), f.getScores);
-
-// function validate(credentials, key) {
-
-//     currentUser = Users.userList.filter(function (value) {
-//         return value.token === credentials.token;
-//     })[0];
-
-//     if (!currentUser) {
-//         currentUser = Users.userList.filter(function (value) {
-//             return (value.username === credentials.username && value.password === credentials.password);
-//         })[0];
-//     }
-
-//     if (currentUser) return (currentUser.permission === key);
-// }
-
-function requireRole(role) {
-    return function (req, res, next) {
-        var a = false,
-            credentials = req.cookies.token ? req.cookies : req.body;
-
-        f.getUser(credentials, function (user) {
-            if (role.indexOf(user && user.permission) != -1) {
-                currentUser = user;
-                next();
-            } else {
-                res.sendStatus(403);
-            }
-        });
-    }
-}
-
-function sendCookies(req, res) {
-    res.send({
-        token: currentUser.token,
-        link: currentUser.permission,
-        UserID: currentUser.id,
-        alert: [{
-            type: 'success',
-            msg: 'Another alert!'
-        }, {
-            type: 'warning',
-            msg: 'Another alert!'
-        }]
-    });
-};
+app.get('/' + Admin, f.requireRole([Admin]), sendHomePage);
+app.get('/' + User, f.requireRole([User]), sendHomePage);
 
 function sendHomePage(req, res) {
     console.log(User);
     res.sendFile(__dirname + '/dist/index.html');
 };
+
+app.post('/login', f.requireRole([Admin, User]), f.sendCookies);
+
+app.get('/students', f.requireRole([Admin, User]), f.getStudents);
+app.post('/students', f.requireRole([Admin]), f.newStudent);
+app.put('/students', f.requireRole([Admin]), f.editStudent);
+app.delete('/students', f.requireRole([Admin]), f.deleteStudent);
+
+app.get('/recomends', f.requireRole([Admin, User]), f.getRecomends);
+app.post('/recomends', f.requireRole([User]), f.newRecomend);
+app.post('/approve', f.requireRole([Admin]), f.approveRecomend);
+app.post('/deny', f.requireRole([Admin]), f.denyRecomend);
+
+app.post('/daily', f.requireRole([Admin, User]), f.getDailyReport);
+//app.put('/daily', f.requireRole([Admin, User]), f.updateDailyReport);
+
+app.post('/scores', f.requireRole([Admin, User]), f.getScores);
 
 var port = process.env.PORT || 8080;
 
