a7c3bb8f9cb84c2cdd131981505cb942b3d2dac0
rabbishuki <rabbishuki@gmail.com>
2017-02-20 23:30:30 +0200

    ADD: Change month for scores

--- src/js/controllers/scoresController.js
+++ src/js/controllers/scoresController.js
@@ -3,20 +3,35 @@ angular.module('RDash')
         $scope.students = [];
         $scope.dropList = {};
         $scope.UPDstud = [];
-        $scope.date = new Date().toLocaleDateString('en-GB').split('/').reverse().join('-');
 
-        Data.get('scores/' + $scope.date).then(function (data) {
-            $scope.students = data.scores;
-            $scope.title = data.test_type;
-            $scope.options = data.options;
-        });
+        $scope.loadData = function (selectedDate) {
+            var date = selectedDate ? new Date(selectedDate) : new Date()
+            $scope.date = date.toLocaleDateString('en-GB').split('/').reverse().join('-');
+
+            Data.get('scores/' + $scope.date).then(function (data) {
+                $scope.students = data.scores;
+                $scope.title = data.test_type;
+                $scope.options = data.options;
+            });
+        }
+        
+        Data.get('prevDates').then(function (data) {
+            $scope.prevDates = data.prevDates;
+        })
+
+        $scope.changeMonth = function (currentMonth) {
+            $scope.loadData(Object.values(JSON.parse(currentMonth)).join('-'))
+        }
 
         $scope.save = function (data) {
-
             $scope.UPDstud = $scope.students.filter((val) => (val.oral || val.oral === 0 || val.write));
-            Data.put('scores', {score : $scope.UPDstud, date: $scope.date}).then(function(data){
+            Data.put('scores', {
+                score: $scope.UPDstud,
+                date: $scope.date
+            }).then(function (data) {
 
             })
-
         }
+
+        $scope.loadData();
     })
