ef3f1e16f952ad9add3c161e8d448abb15e71ddb
rabbishuki <rabbishuki@gmail.com>
2017-07-06 13:01:51 +0300

    FIX: format hours number

--- src/js/filters/timeFilter.js
+++ src/js/filters/timeFilter.js
@@ -1,12 +1,12 @@
-angular.module('RDash').filter('timeFilter', function () {
+angular.module('RDash').filter('timeFilter', function ($filter) {
     return function (input) {
         if (typeof input !== "number" || input < 0) return "00:00";
 
         function pad(n) { return (n < 10 ? '0' : '') + n; }
 
-        var hours = parseInt(input);
-        var minutes = Math.ceil(input % 1 * 60);
+        var hours = pad(parseInt(input));
+        var minutes = pad(Math.ceil(input % 1 * 60));
 
-        return (pad(hours) + ':' + pad(minutes));
+        return ($filter('number')(hours) + ':' + minutes);
     }
 });
