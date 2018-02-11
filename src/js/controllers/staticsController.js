ac1d234aa416d7288b06e92b8bb61751168f0558
shalomdovber <shalomber10@gmail.com>
2018-02-11 23:52:04 +0200

    move data to pc

--- src/js/controllers/staticsController.js
+++ src/js/controllers/staticsController.js
@@ -1,8 +1,32 @@
 angular.module('RDash')
-.controller('staticsController', function ($scope, Data) {
-    $scope.changeStaticsType = function(sortType, staticsType){
-        if(sortType && staticsType) {
-            $scope.secondSec = true;
+    .controller('staticsController', function($scope, Data) {
+        $scope.changeStaticsType = function(sortType, staticsType) {
+            // if (sortType && staticsType) {
+            //     $scope.secondSec = true;
+            // }
         }
-    }
-});
+
+        $scope.getStatics = function() {
+            Data.get('getStatics/' + $scope.staticsType + '/' + $scope.sortType + '/' + $scope.dateType + '/' + amountOfData)
+        }
+
+        // window.onload = function() {
+        // var chart = new CanvasJS.Chart("chartContainer", {
+        //     title: {
+        //         text: "My First Chart in CanvasJS"
+        //     },
+        //     data: [{
+        //         // Change type to "doughnut", "line", "splineArea", etc.
+        //         type: "column",
+        //         dataPoints: [
+        //             { label: "apple", y: 10 },
+        //             { label: "orange", y: 15 },
+        //             { label: "banana", y: 25 },
+        //             { label: "mango", y: 30 },
+        //             { label: "grape", y: 28 }
+        //         ]
+        //     }]
+        // });
+        // chart.render();
+        // }
+    });
