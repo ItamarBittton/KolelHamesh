d691dee6ce913551dbd0dcf14a4fee169fabaf40
shalomdovber <shalomber10@gmail.com>
2018-02-14 10:43:54 +0200

    move to allow rosh colel get a report

--- src/js/controllers/staticsController.js
+++ src/js/controllers/staticsController.js
@@ -7,26 +7,26 @@ angular.module('RDash')
         }
 
         $scope.getStatics = function() {
-            Data.get('getStatics/' + $scope.staticsType + '/' + $scope.sortType + '/' + $scope.dateType + '/' + amountOfData)
+            Data.get('getStatics/' + $scope.staticsType + '/' + $scope.sortType + '/' + $scope.dateType + '/' + $scope.amountOfHistory)
         }
 
         $scope.show = function() {
-        var chart = new CanvasJS.Chart("chartContainer", {
-            title: {
-                text: "My First Chart in CanvasJS"
-            },
-            data: [{
-                // Change type to "doughnut", "line", "splineArea", etc.
-                type: "column",
-                dataPoints: [
-                    { label: "apple", y: 10 },
-                    { label: "orange", y: 15 },
-                    { label: "banana", y: 25 },
-                    { label: "mango", y: 30 },
-                    { label: "grape", y: 28 }
-                ]
-            }]
-        });
-        chart.render();
+            var chart = new CanvasJS.Chart("chartContainer", {
+                title: {
+                    text: "My First Chart in CanvasJS"
+                },
+                data: [{
+                    // Change type to "doughnut", "line", "splineArea", etc.
+                    type: "column",
+                    dataPoints: [
+                        { label: "apple", y: 10 },
+                        { label: "orange", y: 15 },
+                        { label: "banana", y: 25 },
+                        { label: "mango", y: 30 },
+                        { label: "grape", y: 28 }
+                    ]
+                }]
+            });
+            chart.render();
         }
     });
