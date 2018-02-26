91bf622e8ac6a14188dbe321c33335778993da19
shalomdovber <shalomber10@gmail.com>
2018-02-26 20:45:04 +0200

    display chart

--- src/js/controllers/staticsController.js
+++ src/js/controllers/staticsController.js
@@ -1,33 +1,104 @@
 angular.module('RDash')
-    .controller('staticsController', function ($scope, Data) {
-        $scope.changeStaticsType = function (sortType, staticsType) {
+    .controller('staticsController', function($scope, Data) {
+        $scope.changeStaticsType = function(sortType, staticsType) {
             // if (sortType && staticsType) {
             //     $scope.secondSec = true;
             // }
         }
         $scope.statics = {};
 
-        $scope.getStatics = function () {
+        $scope.getStatics = function() {
             var startDate = $scope.statics.startDate.split('/');
             var endDate = $scope.statics.endDate.split('/');
             Data.get('getStatics/' + $scope.statics.staticsType +
                 '/' + new Date(startDate[2], startDate[1], startDate[0]).getTime() +
                 '/' + new Date(endDate[2], endDate[1], endDate[0]).getTime() +
-                '/' + $scope.statics.dateType).then(function(res){
-                    console.log(res);
-                })
+                '/' + $scope.statics.dateType).then(function(res) {
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    title: {
+                        text: "Multi-Series Line Chart"
+                    },
+                    data: [{
+                            type: "line",
+                            showInLegend: true,
+                            dataPoints: [
+                                { x: 10, y: 21 },
+                                { x: 20, y: 25 },
+                                { x: 30, y: 20 },
+                                { x: 40, y: 25 },
+                                { x: 50, y: 27 },
+                                { x: 60, y: 28 },
+                                { x: 70, y: 28 },
+                                { x: 80, y: 24 },
+                                { x: 90, y: 26 }
+
+                            ]
+                        },
+                        {
+                            type: "line",
+                            dataPoints: [
+                                { x: 10, y: 31 },
+                                { x: 20, y: 35 },
+                                { x: 30, y: 30 },
+                                { x: 40, y: 35 },
+                                { x: 50, y: 35 },
+                                { x: 60, y: 38 },
+                                { x: 70, y: 38 },
+                                { x: 80, y: 34 },
+                                { x: 90, y: 44 }
+
+                            ]
+                        },
+                        {
+                            type: "line",
+                            dataPoints: [
+                                { x: 10, y: 45 },
+                                { x: 20, y: 50 },
+                                { x: 30, y: 40 },
+                                { x: 40, y: 45 },
+                                { x: 50, y: 45 },
+                                { x: 60, y: 48 },
+                                { x: 70, y: 43 },
+                                { x: 80, y: 41 },
+                                { x: 90, y: 28 }
+
+                            ]
+                        },
+                        {
+                            type: "line",
+                            dataPoints: [
+                                { x: 10, y: 71 },
+                                { x: 20, y: 55 },
+                                { x: 30, y: 50 },
+                                { x: 40, y: 65 },
+                                { x: 50, y: 95 },
+                                { x: 60, y: 68 },
+                                { x: 70, y: 28 },
+                                { x: 80, y: 34 },
+                                { x: 90, y: 14 }
+
+                            ]
+                        }
+                    ]
+                });
+
+                chart.render();
+            })
         }
 
-        $scope.$on('$viewContentLoaded', function () {
+        $scope.$on('$viewContentLoaded', function() {
             $("#datepicker").datepicker({
                 closeText: "סגור",
                 prevText: "&#x3C;הקודם",
                 nextText: "הבא&#x3E;",
                 currentText: "היום",
                 monthNames: ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני",
-                    "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"],
+                    "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"
+                ],
                 monthNamesShort: ["ינו", "פבר", "מרץ", "אפר", "מאי", "יוני",
-                    "יולי", "אוג", "ספט", "אוק", "נוב", "דצמ"],
+                    "יולי", "אוג", "ספט", "אוק", "נוב", "דצמ"
+                ],
                 dayNames: ["ראשון", "שני", "שלישי", "רביעי", "חמישי", "שישי", "שבת"],
                 dayNamesShort: ["א'", "ב'", "ג'", "ד'", "ה'", "ו'", "שבת"],
                 dayNamesMin: ["א'", "ב'", "ג'", "ד'", "ה'", "ו'", "שבת"],
@@ -44,9 +115,11 @@ angular.module('RDash')
                 nextText: "הבא&#x3E;",
                 currentText: "היום",
                 monthNames: ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני",
-                    "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"],
+                    "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"
+                ],
                 monthNamesShort: ["ינו", "פבר", "מרץ", "אפר", "מאי", "יוני",
-                    "יולי", "אוג", "ספט", "אוק", "נוב", "דצמ"],
+                    "יולי", "אוג", "ספט", "אוק", "נוב", "דצמ"
+                ],
                 dayNames: ["ראשון", "שני", "שלישי", "רביעי", "חמישי", "שישי", "שבת"],
                 dayNamesShort: ["א'", "ב'", "ג'", "ד'", "ה'", "ו'", "שבת"],
                 dayNamesMin: ["א'", "ב'", "ג'", "ד'", "ה'", "ו'", "שבת"],
@@ -59,24 +132,75 @@ angular.module('RDash')
             });
         });
 
-        $scope.show = function () {
+        $scope.show = function() {
 
             var chart = new CanvasJS.Chart("chartContainer", {
                 title: {
-                    text: "My First Chart in CanvasJS"
+                    text: "Multi-Series Line Chart"
                 },
                 data: [{
-                    // Change type to "doughnut", "line", "splineArea", etc.
-                    type: "column",
-                    dataPoints: [
-                        { label: "apple", y: 10 },
-                        { label: "orange", y: 15 },
-                        { label: "banana", y: 25 },
-                        { label: "mango", y: 30 },
-                        { label: "grape", y: 28 }
-                    ]
-                }]
+                        type: "line",
+                        dataPoints: [
+                            { x: 10, y: 21 },
+                            { x: 20, y: 25 },
+                            { x: 30, y: 20 },
+                            { x: 40, y: 25 },
+                            { x: 50, y: 27 },
+                            { x: 60, y: 28 },
+                            { x: 70, y: 28 },
+                            { x: 80, y: 24 },
+                            { x: 90, y: 26 }
+
+                        ]
+                    },
+                    {
+                        type: "line",
+                        dataPoints: [
+                            { x: 10, y: 31 },
+                            { x: 20, y: 35 },
+                            { x: 30, y: 30 },
+                            { x: 40, y: 35 },
+                            { x: 50, y: 35 },
+                            { x: 60, y: 38 },
+                            { x: 70, y: 38 },
+                            { x: 80, y: 34 },
+                            { x: 90, y: 44 }
+
+                        ]
+                    },
+                    {
+                        type: "line",
+                        dataPoints: [
+                            { x: 10, y: 45 },
+                            { x: 20, y: 50 },
+                            { x: 30, y: 40 },
+                            { x: 40, y: 45 },
+                            { x: 50, y: 45 },
+                            { x: 60, y: 48 },
+                            { x: 70, y: 43 },
+                            { x: 80, y: 41 },
+                            { x: 90, y: 28 }
+
+                        ]
+                    },
+                    {
+                        type: "line",
+                        dataPoints: [
+                            { x: 10, y: 71 },
+                            { x: 20, y: 55 },
+                            { x: 30, y: 50 },
+                            { x: 40, y: 65 },
+                            { x: 50, y: 95 },
+                            { x: 60, y: 68 },
+                            { x: 70, y: 28 },
+                            { x: 80, y: 34 },
+                            { x: 90, y: 14 }
+
+                        ]
+                    }
+                ]
             });
+
             chart.render();
         }
     });
